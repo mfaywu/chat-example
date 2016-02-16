96394201e3478dd778930f59eb50484464287e1d
M. Fay Wu <wu.mfay@gmail.com>
2016-02-16 11:10:36 -0800

    Added nickname feature

--- index.js
+++ index.js
@@ -7,16 +7,40 @@ app.get('/', function(req, res) {
     });
 
 io.on('connection', function(socket) { 
-	//console.log('a user connected');
-	//socket.broadcast.emit('chat message', ' connected');
+
+	var username;
 	
 	socket.on('connected message', function(msg) {
-		console.log('socket.id is ' + msg);
-		socket.broadcast.emit('chat message', msg + ' is connected!');
+		username = socket.id;
+		console.log('client ' + username + ' has connected');
+		socket.broadcast.emit('chat message', username + ' is connected!');
+		socket.emit('chat message', 'You have joined the chat channel!');
 	    });
 
 	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
+		if (msg.charAt(0) == '/') {
+		    var pos = msg.indexOf(":");
+		    var cmd = msg.substring(1, pos);
+		    switch (cmd) {
+		    case "username":
+			var prev_username = username;
+			username = msg.substring(11, msg.length);
+			socket.broadcast.emit('chat message', prev_username + ' changed their username to ' + username);
+			socket.emit('chat message', 'You have changed your username to: ' + username);
+			break;
+		    case "sendto":
+			var sub_msg = msg.substring(pos+2, msg.length);
+			var pos1 = newmsg.indexOf(' ');
+			var receiver = sub_msg.substring(0, pos1);
+			//			socket.broadcast.to(receiver).emit('chat message', 
+			break;
+		    default:
+			socket.emit('chat message', 'Invalid command.');
+			break;
+		    }
+		} else {
+		 	 io.emit('chat message', msg);
+		}
 	    });
 	socket.on('disconnect', function(msg) {
 		//console.log('user disconnected');
